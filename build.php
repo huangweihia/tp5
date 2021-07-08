61f778fe4f3435c9026f917880820b25bb1632d8
Make Money <2801359160@qq.com>
2021-07-08 17:47:19 +0800

    修改静态文件目录名称

--- build.php
+++ build.php
@@ -14,7 +14,7 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
+    'admin'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
         'controller' => ['Index', 'Test', 'UserType'],
@@ -23,3 +23,4 @@ return [
     ],
     // 其他更多的模块定义
 ];
+
